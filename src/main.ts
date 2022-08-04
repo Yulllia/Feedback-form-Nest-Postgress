4dc8f290bb7dd70b674339dc58ca5df8af493da9
Yulia Shyshka <shyshka20@gmail.com>
2022-08-05 01:00:57 +0300

    add

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 if(!process.env.IS_TS_NODE){
   require('module-alias/register')
@@ -7,8 +11,11 @@ if(!process.env.IS_TS_NODE){
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors:true});
-  await app.listen(process.env.PORT || 5432);
-  const httpAdapter = app.getHttpAdapter();
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+
+  await app.listen(process.env.PORT || '0.0.0.0');
 }
 bootstrap();
